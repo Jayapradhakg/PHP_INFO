66fe3f42acad3d11250ee107bf19088241a33c56
Jayapradhakg <jayapradha@gmail.com>
2017-04-24 11:24:07 -0700

    Update single_vs_double_quote.php

--- single_vs_double_quote.php
+++ single_vs_double_quote.php
@@ -63,3 +63,76 @@ foreach ($values as $value) {
 }
 
 ?>
+
+/**
+jkodihalli@jayamac ~/tmp (42) {master} % php single_vs_double_quote.php 
+ != comparison of 2 strings '' and  "" 
+string match 
+ !== comparison of 2 strings '' and  "" 
+string match 
+
+ Please enter the string 
+
+string(0) ""
+ vardump of inputstring(0) ""
+ vardump of single_qstring(0) ""
+ vardump of double_q != comparison of input string and "" 
+string match 
+ != comparison of input string and '' 
+string match 
+ !== comparison of input string and ""
+string match 
+ != comparison of input string and '' 
+string match 
+
+ is_string() function return values check 
+is_string(false) = bool(false)
+is_string(true) = bool(false)
+is_string(NULL) = bool(false)
+is_string('abc') = bool(true)
+is_string('23') = bool(true)
+is_string(23) = bool(false)
+is_string('23.5') = bool(true)
+is_string(23.5) = bool(false)
+is_string('') = bool(true)
+is_string('') = bool(true)
+is_string(' ') = bool(true)
+is_string('0') = bool(true)
+is_string(0) = bool(false)
+is_string('') = bool(true)
+jkodihalli@jayamac ~/tmp (43) {master} % php single_vs_double_quote.php 
+ != comparison of 2 strings '' and  "" 
+string match 
+ !== comparison of 2 strings '' and  "" 
+string match 
+
+ Please enter the string 
+jaya
+string(4) "jaya"
+ vardump of inputstring(0) ""
+ vardump of single_qstring(0) ""
+ vardump of double_q != comparison of input string and "" 
+string dont match 
+ != comparison of input string and '' 
+string dont match 
+ !== comparison of input string and ""
+string dont match 
+ != comparison of input string and '' 
+string dont match 
+
+ is_string() function return values check 
+is_string(false) = bool(false)
+is_string(true) = bool(false)
+is_string(NULL) = bool(false)
+is_string('abc') = bool(true)
+is_string('23') = bool(true)
+is_string(23) = bool(false)
+is_string('23.5') = bool(true)
+is_string(23.5) = bool(false)
+is_string('') = bool(true)
+is_string('') = bool(true)
+is_string(' ') = bool(true)
+is_string('0') = bool(true)
+is_string(0) = bool(false)
+is_string('jaya') = bool(true)
+**/
